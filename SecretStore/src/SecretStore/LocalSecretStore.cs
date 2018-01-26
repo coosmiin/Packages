124dc1152610bf9fa22570babc780a331cc4970e
coosmiin <coosmiin@yahoo.com>
2018-01-26 19:03:01 +0200

    Create SecretStore solution

---
+++ SecretStore/src/SecretStore/LocalSecretStore.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SecretStore
+{
+	public class LocalSecretStore<T> : ISecretStore where T : class
+	{
+		IConfigurationRoot _configurationRoot;
+
+		public LocalSecretStore()
+		{
+			_configurationRoot = BuildConfiguration();
+		}
+
+		public string GetSecret(string key)
+		{
+			return _configurationRoot[key];
+		}
+
+		private static IConfigurationRoot BuildConfiguration()
+		{
+			IConfigurationBuilder builder = new ConfigurationBuilder();
+
+			builder.AddUserSecrets<T>();
+
+			IConfigurationRoot configuration = builder.Build();
+
+			return configuration;
+		}
+	}
+}
